b667a26288ed99aff7f650832d823fcbe1690c7f
Fabian <f.vandenbroek@cs.ru.nl>
2017-04-19 16:59:30 +0200

    removed cardreader symbol from dialog

--- client/irma.js
+++ client/irma.js
@@ -247,7 +247,7 @@ function doInitialRequest(request, contenttype, success_cb, cancel_cb, failure_c
             serverPage = "verify.html";
         else
             serverPage = "sign.html";
-        popup = window.open(webServer + serverPage, 'name','height=400,width=640');
+        popup = window.open(webServer + serverPage, 'name','height=400,width=400');
         if (window.focus) {
             popup.focus();
         }
