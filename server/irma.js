5c19749acb14add7d95a8bc20cb42e645c8f1d0d
Sietse Ringers <mail@sietseringers.net>
2017-09-26 11:14:14 +0200

    Execute server javascript immediately

--- server/irma.js
+++ server/irma.js
@@ -1,55 +1,58 @@
-$(function() {
-    function handleMessage(event) {
-        if (event.source !== window.parent) {
-            console.warn("Warning: discarding message from other source than caller: ", event);
-            return;
-        }
+function handleMessage(event) {
+    var msg = event.data;
+    console.log("Received message: ", msg);
 
-        console.log("Received message: ", event);
-        var msg = event.data;
-
-        switch (msg.type) {
-            case "tokenData":
-                console.log("Got a QR code");
-                $("#qrcode").empty().qrcode({
-                    text: JSON.stringify(msg.message),
-                    size: 230,
-                });
-                $("#spinner").hide();
-                $(".irma_option_container").show();
-                break;
-            case "clientConnected":
-                showMessage("Please follow the instructions on your IRMA token");
-                $(".irma_option_container").hide();
-                break;
-            case "done":
-                break;
-            default:
-                console.warn("Received unknown message: \"", msg, "\"");
-                break;
-        }
+    switch (msg.type) {
+        case "tokenData":
+            console.log("Got a QR code");
+            $("#qrcode").empty().qrcode({
+                text: JSON.stringify(msg.message),
+                size: 230,
+            });
+            $("#spinner").hide();
+            $(".irma_option_container").show();
+            break;
+        case "clientConnected":
+            showMessage("Please follow the instructions on your IRMA token");
+            $(".irma_option_container").hide();
+            break;
+        case "done":
+            break;
+        default:
+            failure("Received unknown message: \"" + msg + "\"");
+            break;
     }
+}
 
-    function sendMessage(data) {
-        window.parent.postMessage(data, "*");
-        console.log("Sent message: ", JSON.stringify(data));
+function sendMessage(data){
+    window.top.postMessage(data, "*");
+    console.log("Sent message: " + JSON.stringify(data));
+}
+
+function failure() {
+    console.log("ERROR: ", arguments);
+
+    if (arguments.length > 0) {
+        $(".irma_title").html("ERROR");
+        showMessage("<b>Error: <b> " + arguments[0]);
+        $("#irma_text").add_class("error");
     }
+}
 
-    function showMessage(msg) {
-        $("#irma_text").html(msg);
-    }
+function showMessage(msg) {
+    $("#irma_text").html(msg);
+}
 
-    $("#cancel_button").on("click", function() {
-        sendMessage({
-            type: "userCancelled",
-        });
-    });
-
-    window.addEventListener("message", handleMessage, false);
-
+$("#cancel_button").on("click", function() {
     sendMessage({
-        type: "serverPageReady",
+        type: "userCancelled",
     });
-
-    console.log("Server module loaded");
 });
+
+window.addEventListener("message", handleMessage, false);
+
+sendMessage({
+    type: "serverPageReady",
+});
+
+console.log("Server module loaded");
