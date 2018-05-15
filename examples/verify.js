474508d971eb27df64f0b59e1676928bc6b35f8f
David Venhoek <david@venhoek.nl>
2018-05-15 13:14:25 +0200

    Updated sign and verify examples to handle the status codes in their error and cancel functions.

--- examples/verify.js
+++ examples/verify.js
@@ -27,13 +27,14 @@ $(function() {
             $("#token-raw").text(JSON.stringify(data));
             $("#token-content").text(JSON.stringify(jwt_decode(data), null, 2));
         }
-        var cancel_fun = function(data) {
+        var cancel_fun = function() {
             console.log("Authentication cancelled!");
+            console.log("Cancel data:", arguments);
             $("#result_status").html("Cancelled!");
         }
-        var error_fun = function(data) {
+        var error_fun = function() {
             console.log("Authentication failed!");
-            console.log("Error data:", data);
+            console.log("Error data:", arguments);
             $("#result_status").html("Failure!");
         }
 
